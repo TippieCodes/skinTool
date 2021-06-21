84c725f645898aa37421888490be8036e8a852ea
Tippie <rowan@tippie.me>
2021-06-21 16:30:35 +0200

    Removed GetSkinCommand.java

--- src/main/java/me/tippie/skintool/SkinTool.java
+++ src/main/java/me/tippie/skintool/SkinTool.java
@@ -1,6 +1,5 @@
 package me.tippie.skintool;
 
-import me.tippie.skintool.commands.GetSkinCommand;
 import me.tippie.skintool.commands.SaveSkinCommand;
 import me.tippie.skintool.commands.SetSkinCommand;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,7 +8,6 @@ public final class SkinTool extends JavaPlugin {
 	private static SkinTool instance;
 	@Override
 	public void onEnable() {
-		this.getCommand("getskin").setExecutor(new GetSkinCommand());
 		this.getCommand("saveskin").setExecutor(new SaveSkinCommand());
 		this.getCommand("setskin").setExecutor(new SetSkinCommand());
 		instance = this;
